a5b4cbac853786ff26f52f19e2ada7edf0bd06e7
RoxanaCS <roxale.cares@gmail.com>
2017-12-05 18:29:31 -0300

    header, seccion1 2 3

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 // La funcionalidad de tu proyecto
+myMap()
+function myMap() {
+var mapOptions = {
+   center: new google.maps.LatLng(-10, -76),
+   zoom: 2,
+   mapTypeId: google.maps.MapTypeId.ROADMAP 
+}
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
